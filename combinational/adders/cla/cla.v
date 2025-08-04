0edbbb580b0af47b3e517eaf6d6689772f509d08
eziocanswim <samyakpandey2007@gmail.com>
2025-08-04 21:17:44 +0530

    meow

--- combinational/adders/cla/cla.v
+++ combinational/adders/cla/cla.v
@@ -1,1 +1,10 @@
 // 4-bit carry look ahead adder [Generate and Propagate focussed]
+
+module cla4bit (
+    output [3:0] SUM,
+    output carry,
+    input [3:0] A, B,
+    input C0
+);
+    
+endmodule
